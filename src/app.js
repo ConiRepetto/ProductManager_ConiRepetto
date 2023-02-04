f0e83862bde22f9fd0bc3c39f3adb27c110a548c
Constanza Repetto <repettocj@gmail.com>
2023-02-04 11:58:40 -0300

    cambios

--- src/app.js
+++ src/app.js
@@ -2,50 +2,61 @@ import express from 'express'
 
 const app = express();
 
-const frase = "Holaa Coniii como estas hoy ?"
-
-app.get('/api/getFrase', (req, res) => {
-    res.send(frase) //response ante la request sera enviar frase
+//Rutas del servidor
+app.get('/bienvenida', (req, res) => {
+    res.send(`<html><body><p style="color: blue">HOla Coni!</p></body></html>`) //response ante la request sera enviar frase
 })
 
-//Devolver letra segun su numero de posicion
-app.get('/api/getLetra/:num', (req, res) => {
-    let num = parseInt(req.params.num)
-    if ((!isNaN(num))) {
-        if (num >= 1 && num <= frase.length) {
-            res.send(frase[num - 1]) //-1 porque?
-        } else {
-            res.send({
-                error: "El parametro esta fuera de rango"
-            })
-        }
-    } else {
-        res.send({
-            error: "El parametro no es un numero"
-        })
-    }
-
+app.get('/usuario', (req, res) => {
+    const usuario = {nombre: "Coni", apellido: "Repetto", edad: 27, correo: "bla bla"};
+    res.send(usuario)
 })
 
-//Devolver palabra segun su numero de posicion
-app.get('/api/getPalabra/:num', (req, res) => {
-    let num = parseInt(req.params.num)
-    if ((!isNaN(num))) {
-        let palabras = frase.split(' ') //separar la frase en los espacios, crea un array
-        if (num >= 1 && num <= frase.length) {
-            res.send(palabras[num - 1]) //-1 porque?
-        } else {
-            res.send({
-                error: "El parametro esta fuera de rango"
-            })
-        }
-    } else {
-        res.send({
-            error: "El parametro no es un numero"
-        })
-    }
 
-})
+// const frase = "Holaa Coniii como estas hoy ?"
+
+// app.get('/api/getFrase', (req, res) => {
+//     res.send(frase) //response ante la request sera enviar frase
+// })
+
+// //Devolver letra segun su numero de posicion
+// app.get('/api/getLetra/:num', (req, res) => {
+//     let num = parseInt(req.params.num)
+//     if ((!isNaN(num))) {
+//         if (num >= 1 && num <= frase.length) {
+//             res.send(frase[num - 1]) //-1 porque?
+//         } else {
+//             res.send({
+//                 error: "El parametro esta fuera de rango"
+//             })
+//         }
+//     } else {
+//         res.send({
+//             error: "El parametro no es un numero"
+//         })
+//     }
+
+// })
+
+// //Devolver palabra segun su numero de posicion
+// app.get('/api/getPalabra/:num', (req, res) => {
+//     let num = parseInt(req.params.num)
+//     if ((!isNaN(num))) {
+//         let palabras = frase.split(' ') //separar la frase en los espacios, crea un array
+//         if (num >= 1 && num <= frase.length) {
+//             res.send(palabras[num - 1]) //-1 porque?
+//         } else {
+//             res.send({
+//                 error: "El parametro esta fuera de rango"
+//             })
+//         }
+//     } else {
+//         res.send({
+//             error: "El parametro no es un numero"
+//         })
+//     }
+
+// })
 
 
 
@@ -56,8 +67,7 @@ app.get('/api/getPalabra/:num', (req, res) => {
 
 
 //Configurar Servidor
-const PORT = 8080
-const server = app.listen(PORT, () => {
-    console.log('Servidor ejecutandose en el puerto: ', PORT)
+const port = 8080
+app.listen(port, () => {
+    console.log('Servidor ejecutandose en el puerto: ', port)
 })
-server.on('error', error => console.log('Error en el servidor: ', error))
